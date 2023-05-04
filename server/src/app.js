38845141202ce48a47a6a2b6ba53a15cbb3e128f
peter varga <pkvarg@yahoo.se>
2023-05-04 13:24:10 +0200

    192

--- server/src/app.js
+++ server/src/app.js
@@ -3,22 +3,21 @@ const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 
-const planetsRouter = require('./routes/planets/planets.router.js')
-const launchesRouter = require('./routes/launches/launches.router.js')
+const api = require('./routes/api')
 
 const app = express()
 
 app.use(
   cors({
     origin: 'http://localhost:3000',
+    origin: 'http://localhost:8000',
   })
 )
-app.use(morgan('dev'))
+app.use(morgan('combined'))
 
 app.use(express.json())
 app.use(express.static(path.join(__dirname, '..', 'public')))
-app.use('/planets', planetsRouter)
-app.use('/launches', launchesRouter)
+app.use('/v1', api)
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
 })
