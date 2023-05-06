8c7ce4e19d512195c394f75ef23a2a18d52d8d75
peter varga <pkvarg@yahoo.se>
2023-05-06 16:44:57 +0200

    test planets

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,10 +1,13 @@
 const request = require('supertest')
 const app = require('../../app')
 const { mongoConnect, mongoDisconnect } = require('./../../services/mongo')
+const { loadLaunchesData } = require('../../models/launches.model')
+const { loadPlanetsData } = require('../../models/planets.model')
 
 describe('LAUNCHES API', () => {
   beforeAll(async () => {
     await mongoConnect()
+    await loadPlanetsData()
   })
 
   afterAll(async () => {
